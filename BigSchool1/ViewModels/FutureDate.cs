47d8bd931a78479b4ae8248f19bf72cdee320844
Administrator <Administrator@PC201>
2020-06-09 10:18:45 +0700

    Add project files.

---
+++ BigSchool1/ViewModels/FutureDate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+
+namespace BigSchool1.ViewModels
+{
+    public class FutureDate : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            DateTime dateTime;
+            var isVaLid = DateTime.TryParseExact(Convert.ToString(value),
+                "dd/M/yyyy",
+                CultureInfo.CurrentCulture,
+                DateTimeStyles.None,
+                out dateTime);
+            return (isVaLid && dateTime > DateTime.Now);
+        }
+    }
+}
